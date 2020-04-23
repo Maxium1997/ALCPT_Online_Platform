f521a8bb621272beae71393f3afc6c243718187c
reSetRoot <wei860925@gmail.com>
2020-04-24 00:48:00 +0800

    Added the permission check before user_list and view_user_detail

--- alcpt/views/SystemManager/user.py
+++ alcpt/views/SystemManager/user.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 
 from registration.models import User
 from registration.definition import Privilege
@@ -7,9 +10,16 @@ from registration.definition import Privilege
 # Create your views here.
 
 
+@method_decorator(login_required, name='dispatch')
 class UserListView(ListView):
     model = User
     template_name = 'SystemManager/user/list.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        required_privilege = Privilege.SystemManager
+        if not request.user.has_permission(required_privilege):
+            raise PermissionDenied
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -18,7 +28,14 @@ class UserListView(ListView):
         return context
 
 
+@method_decorator(login_required, name='dispatch')
 class UserDetailView(DetailView):
     model = User
     template_name = 'SystemManager/user/detail.html'
     context_object_name = 'user'
+
+    def dispatch(self, request, *args, **kwargs):
+        required_privilege = Privilege.SystemManager
+        if not request.user.has_permission(required_privilege):
+            raise PermissionDenied
+        return super(UserDetailView, self).dispatch(request, *args, **kwargs)
